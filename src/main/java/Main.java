6b654d0a37d44afd384d63fe6d8c8d7531916979
Minseong Moon <41772872+SD810@users.noreply.github.com>
2019-04-08 13:02:01 +0900

    add safety measures

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ResizeImageJava Main");
 
+        if(args.length <= 0){
+            System.out.println("no args provided. program will exit.");
+            return ;
+        }
         String pathname = args[0];
-        File folder = new File(pathname);
-        File[] listOfFiles = folder.listFiles();
 
-        ResizeProcessor.resizeImagesWithThese(listOfFiles,512,512);
+        if(pathname == null || pathname.length() <= 1){
+            System.out.println("for safety reason, you cannot process root.  program will exit.");
+            return ;
+        } else {
+            File folder = new File(pathname);
+            if( !
+                    (folder.getAbsolutePath().contains("Pictures")
+                     && folder.getAbsolutePath().contains("Screenshots"))
+            ){
+                System.out.println("Only works for customed location, for testing.  program will exit.");
+                return ;
+            }
+
+            File[] listOfFiles = folder.listFiles();
+            int processedFiles = ResizeProcessor.resizeImagesWithThese(listOfFiles, 512, 512);
+
+            System.out.println(processedFiles +"has been processed");
+        }
     }
 }
